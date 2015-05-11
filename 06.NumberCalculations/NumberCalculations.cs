8098e39988134bf952dd8c9f29e2e087eea8ccc7
Elena Bojilova <elena_bogoeva2003@yahoo.co.uk>
2015-05-11 16:31:30 +0300

    changes to 06.NumberCalculations

--- 06.NumberCalculations/NumberCalculations.cs
+++ 06.NumberCalculations/NumberCalculations.cs
@@ -1,59 +1,56 @@
 ﻿using System;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 class NumberCalculations
 {
     static void Main(string[] args)
     {
-        int[] nums = { 1, 3, 4, 5, 1, 0, 5 };
+        double[] nums = { 1, 3, 4, 5, 1, 0.000000000003, 5 };
         Console.WriteLine("Round Numbers");
         Console.WriteLine("Min:{0}, Max:{1}, Sum:{2}, Average:{3}", Min(nums), Max(nums), Sum(nums), Average(nums));
-        Console.WriteLine("Min:{0}, Max:{1}, Sum:{2}, Average:{3}", nums.Min(), nums.Max(), nums.Sum(), nums.Average());
+        //Console.WriteLine("Min:{0}, Max:{1}, Sum:{2}, Average:{3}", nums.Min(), nums.Max(), nums.Sum(), nums.Average());
         decimal[] numsD = { 1.1M, 3.3M, 4.4M, 5.5M, 1.1M, 0.000000003M, 5.2M };
         Console.WriteLine("Float Numbers");
         Console.WriteLine("Min:{0}, Max:{1}, Sum:{2}, Average:{3}", Min(numsD), Max(numsD), Sum(numsD), Average(numsD));
-        Console.WriteLine("Min:{0}, Max:{1}, Sum:{2}, Average:{3}", numsD.Min(), numsD.Max(), numsD.Sum(), numsD.Average());
+        //Console.WriteLine("Min:{0}, Max:{1}, Sum:{2}, Average:{3}", numsD.Min(), numsD.Max(), numsD.Sum(), numsD.Average());
     }
 
-    private static int Min(int[] nums)
+    private static double Min(double[] nums)
     {
+        double min = double.MaxValue;
         for (int j = 1; j < nums.Length; j++)
         {
-            if (nums[0] > nums[j])
+            if (min > nums[j])
             {
-                nums[0] = nums[0] + nums[j];
-                nums[j] = nums[0] - nums[j];
-                nums[0] = nums[0] - nums[j];
+                min = nums[j];
             }
         }
-        int min = nums[0];
         return min;
     }
 
-    private static int Max(int[] nums)
+    private static double Max(double[] nums)
     {
+        double max = double.MinValue;
         for (int j = 1; j < nums.Length; j++)
         {
-            if (nums[0] < nums[j])
+            if (max < nums[j])
             {
-                nums[0] = nums[0] + nums[j];
-                nums[j] = nums[0] - nums[j];
-                nums[0] = nums[0] - nums[j];
+                max = nums[j];
             }
         }
-        int max = nums[0];
         return max;
     }
 
-    private static double Average(int[] nums)
+    private static double Average(double[] nums)
     {
         var average = (double)Sum(nums) / nums.Length;
         return average;
     }
 
-    private static int Sum(int[] nums)
+    private static double Sum(double[] nums)
     {
-        int sum = 0;
+        double sum = 0;
         for (int i = 0; i < nums.Length; i++)
         {
             sum += nums[i];
@@ -63,38 +60,33 @@ class NumberCalculations
 
     private static decimal Min(decimal[] nums)
     {
-        decimal temp;
+        decimal min = decimal.MaxValue;
         for (int j = 1; j < nums.Length; j++)
         {
-            if (nums[0] > nums[j])
+            if (min > nums[j])
             {
-                temp = nums[0];
-                nums[0] = nums[j];
-                nums[j] = temp;
+                min = nums[j];
             }
         }
-        decimal min = nums[0];
         return min;
     }
 
     private static decimal Max(decimal[] nums)
     {
+        decimal max = decimal.MinValue;
         for (int j = 1; j < nums.Length; j++)
         {
-            if (nums[0] < nums[j])
+            if (max < nums[j])
             {
-                nums[0] = nums[0] + nums[j];
-                nums[j] = nums[0] - nums[j];
-                nums[0] = nums[0] - nums[j];
+                max = nums[j];
             }
         }
-        decimal max = nums[0];
         return max;
     }
 
     private static decimal Average(decimal[] nums)
     {
-        var average = Sum(nums) / nums.Length;
+        var average = (decimal)Sum(nums) / nums.Length;
         return average;
     }
 
@@ -107,5 +99,4 @@ class NumberCalculations
         }
         return sum;
     }
-
 }
